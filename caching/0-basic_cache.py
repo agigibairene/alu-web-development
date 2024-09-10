cc13fe55edb1cb5fc55f931a816dcd3dcff1802d
root <i.agigiba@alustudent.com>
2024-09-10 07:06:54 +0000

    Caching

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""0. Basic dictionary"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Create BasicCache class that Inherits from BaseCaching."""
+
+    def put(self, key, item):
+        """ Must assign to the dictionary self.cache_data the item value for the key"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Must return the value in self.cache_data linked to key."""
+        return self.cache_data.get(key, None)
